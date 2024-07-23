9153df0045c8540fb6269373dbc3416e98d260d4
selvi-spec <Selvi.Radhakrishnan@ibm.com>
2024-07-24 00:32:04 +0530

    Add files via upload

---
+++ Python/Activities/activity23_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_sum(num_list):
+
+    # Initialize sum
+    sum = 0
+
+    # Add number in the list
+    for n in num_list:
+        sum += n
+
+    # Assertion
+    assert sum == 55
